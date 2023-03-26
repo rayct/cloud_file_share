80a081cd0c244993e5fe1c11517b9dab00331d6c
rayct <rayct@icloud.com>
2023-03-26 21:48:00 +0100

    README.md Edit

--- main.py
+++ main.py
@@ -1,40 +1,37 @@
 # 365 Day's of Python Coding Challenge 
-## Day 128: Cloud File Sharing using Python
-## Upload As many as 50 files at once
+# Day 128: Cloud File Sharing using Python
+# Upload As many as 50 files at once
 
-## An Improved Version of the Original.
-
-import gofile as go
-
-def Store_Files(file):
-    cur_server = go.getServer()
-    print(cur_server)
-    url = go.uploadFile(file)
-    if url is not None:
-        print("Download Link: ", url["downloadPage"])
-    else:
-        print("Error: Upload failed.")
-
-Store_Files("test.jpeg")
-
-# #rayturner.dev 
-# # clcoding.com
-
-
-
-
-
-
-# Original Code: 
-# main.py problem: Object of type "None" is not subscriptablePylance
+## My Improved Version of the Original.
 
 # import gofile as go
+
 # def Store_Files(file):
 #     cur_server = go.getServer()
 #     print(cur_server)
 #     url = go.uploadFile(file)
-#     print("Download Link: ", url["downloadPage"])
-# Store_Files("Fm0ejCGXkAEtLUk.jpeg") 
+#     if url is not None:
+#         print("Download Link: ", url["downloadPage"])
+#     else:
+#         print("Error: Upload failed.")
+
+# Store_Files("test.jpeg")
+
+#rayturner.dev 
+#clcoding.com
+
+# Original Code: 
+# main.py problem: Object of type "None" is not subscriptablePylance
+
+
+import gofile as go
+def Store_Files(file):
+    cur_server = go.getServer()
+    print(cur_server)
+    url = go.uploadFile(file)
+    print("Download Link: ", url["downloadPage"])
+Store_Files("test.jpeg") 
+
 
 # The error message "Object of type 'None' is not subscriptable" usually means that you are trying to access a subscript (such as an index or a key) of a variable that has a value of None.
 # In the code you have provided, the variable url is initialized by calling the go.uploadFile(file) function.
@@ -54,7 +51,7 @@ Store_Files("test.jpeg")
 #     else:
 #         print("Error: Upload failed.")
 
-# Store_Files("Fm0ejCGXkAEtLUk.jpeg")
+# Store_Files("test.jpeg")
 
 # This code will print the download link if the upload was successful, or an error message if the upload failed.
 
